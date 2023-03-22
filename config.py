217e74ecfdd7e31ceded045a3b8795779190e85f
Abiuthan Kuruthas <123639416+jlsabi@users.noreply.github.com>
2023-03-22 02:53:52 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+API_KEY = 'your_twitter_api_key'
+API_SECRET_KEY = 'your_twitter_api_secret_key'
+ACCESS_TOKEN = 'your_twitter_access_token'
+ACCESS_TOKEN_SECRET = 'your_twitter_access_token_secret'
+
+FACEBOOK_APP_ID = 'your_facebook_app_id'
+FACEBOOK_APP_SECRET = 'your_facebook_app_secret'
+
+INSTAGRAM_APP_ID = 'your_instagram_app_id'
+INSTAGRAM_APP_SECRET = 'your_instagram_app_secret'
+INSTAGRAM_ACCESS_TOKEN = 'your_instagram_access_token'
